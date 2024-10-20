d9724a1a9923e040ba7c4c6e41afef4bdd2bf6a0
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-10-20 10:50:41 +0500

    App updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import './App.css';
 import Card from './components/Card.jsx';
 import Deck from './components/Deck.jsx';
 import Foundations from './components/Foundations.jsx';
+import Tablu from './components/Tablu.jsx';
+import dec from './Backend/Deck.js'
+import card from './Backend/card.js'
+import Tableu from './Backend/Tableu.js';
+import { useState,useEffect } from 'react';
 function App() {
+const [deck,setDeck] = useState(new dec())
+const [isSet,setcards] = useState(false)
+const [tableu,setTableu] = useState(null)
+  useEffect(() => {
+    deck.shuffleDeck();
+    deck.DisplayCards();
+    const newTableu = new Tableu(deck);
+    newTableu.initializeTableu();
+    setTableu(newTableu);
+    console.log(deck.top)
+  },[]) 
   return (
-    <div className="w-full h-lvh bg-gray-500 flex flex-col">
-      <div className='w-full h-[30%] bg-green-600 flex flex-row gap-64 items-center'>
-        <Deck/>
+    <div className="w-full h-lvh bg-green-700 flex flex-col gap-5">
+      <div className='w-full h-[30%] bg-green-700 flex flex-row gap-64 items-center'>
+        <Deck Dec = {deck}/>
         <Foundations/>
       </div>
-      <div className='w-full h-[70%] border border-black flex flex-col'>
+      <div className='w-full h-[70%] flex flex-col'>
+       setcards ? 
+       {console.log(isSet)}
+      <Tablu tableu = {tableu}/>
+      :
+      null
       </div>
       {/* <Card suit="club" rank="2" faceUp={true} onClick={() => console.log('Card clicked!')}/> */}
+     
     </div>
   );
 }
